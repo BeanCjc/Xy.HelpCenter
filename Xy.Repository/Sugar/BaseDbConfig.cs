ea4e96e525eaee913038c4421ee0dce0080b4181
BeanCjc <1175484775@qq.com>
2019-02-12 22:22:19 +0800

    complete base servicse

---
+++ Xy.Repository/Sugar/BaseDbConfig.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xy.Repository.Sugar
+{
+    public class BaseDbConfig
+    {
+        public static string ConnectionString = "server=.;uid=sa;pwd=123456;database=HelpCenterDB";
+    }
+}
