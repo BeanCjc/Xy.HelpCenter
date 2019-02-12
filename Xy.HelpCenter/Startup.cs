0ed1617ffc8ef272bf04bb44698a00e72d869887
BeanCjc <1175484775@qq.com>
2019-02-12 22:38:37 +0800

    connectionString

--- Xy.HelpCenter/Startup.cs
+++ Xy.HelpCenter/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Xy.Repository.Sugar;
 
 namespace Xy.HelpCenter
 {
@@ -24,6 +25,7 @@ namespace Xy.HelpCenter
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            BaseDbConfig.ConnectionString = Configuration.GetSection("AppSettings:SqlServerConnection").Value;
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
--- Xy.Repository/Sugar/BaseDbConfig.cs
+++ Xy.Repository/Sugar/BaseDbConfig.cs
@@ -6,6 +6,7 @@ namespace Xy.Repository.Sugar
 {
     public class BaseDbConfig
     {
-        public static string ConnectionString = "server=.;uid=sa;pwd=123456;database=HelpCenterDB";
+        public static string ConnectionString { get; set; }
+        //public static string ConnectionString = "server=.;uid=sa;pwd=123456;database=HelpCenterDB";
     }
 }
